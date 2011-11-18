6c1f8ce096746100d89292622c35f8aa3265490f
Vaz Allen <vaz@tryptid.com>
2011-11-18 15:17:07 -0500

    Started product variants.

--- tests/catalog/product_spec.py
+++ tests/catalog/product_spec.py
@@ -1,118 +1,88 @@
 # encoding: utf-8
 
-import mongoengine as db
-
-from nose.tools import *
-
 from shoplifter.catalog.product.model import Product
-
-
-db.connect('shoplifter_test')
-
-
-def setup():
-    Product.drop_collection()
-
-
-def create_product(save=True, force_insert=False, **kwargs):
-    opts = dict(name='Shoplifter T-Shirt')
-    opts.update(kwargs)
-    p = Product(**opts)
-    if save:
-        p.save(force_insert=force_insert)
-    return p
+from tests.helpers import *
+from . import db
+from .helpers import create_product
 
 
 class ProductSpec(object):
-    def _save_with(self, **kwargs):
+    def resave(self, **kwargs):
         "Resave the Product in self.p with some fields changed."
         for k, v, in kwargs.items():
             setattr(self.p, k, v)
         self.p.save()
 
     def setup(self):
-        create_product(code='SHOPTEE')
-        self.p = Product.objects.get(code="SHOPTEE")
+        self.code = 'SHOPTEE'
+        self.name = 'Shoplifter T-Shirt'
+        self.slug = 'shoplifter-t-shirt'
+        Product._test_defaults = dict(name=self.name)
+
+        create_product(code=self.code)
+        self.p = Product.objects.get(code=self.code)
     
     def it_is_identified_by_a_unique_code(self):
         assert_equal(self.p.code, self.p.id)
 
-        qs = Product.objects(code='SHOPTEE')
-        assert_equal(qs.count(), 1)  # one exists
-
-        p = qs.get()  # get it
-        assert_equal(p.code, 'SHOPTEE') # check it
-
-        # test creating again, should not create a new one
-        create_product(code="SHOPTEE")
-        qs2 = Product.objects(code='SHOPTEE')
-        assert_equal(qs2.count(), 1)
-        p2 = qs2.get()
-        assert_equal(p, p2)
-
+        assert_equal(Product.objects(code=self.code).count(), 1)
+        create_product(code=self.code)
+        assert_equal(Product.objects(code=self.code).count(), 1)
         assert_raises(db.OperationError,
-                create_product, code="SHOPTEE", force_insert=True)
+                create_product, code=self.code, force_insert=True)
 
     def it_must_have_a_code_between_3_and_30_characters_long(self):
-        assert_equal(self.p.code, "SHOPTEE")
+        assert_equal(self.p.code, self.code)
 
         # new name for each new saved product, for unique slug
-        def name():
-            for i in xrange(10):
-                yield str(i) * 3
-        name = name()
+        names = (str(i) * 3 for i in xrange(10))
 
-        # creating new products each time because code is immutable (pk)
-        assert_raises(db.ValidationError,
-                create_product, code=None,  name=name.next())
-        assert_raises(db.ValidationError,
-                create_product, code='',    name=name.next())
-        assert_raises(db.ValidationError,
-                create_product, code='aa',  name=name.next())
-        create_product(code='aaa',  name=name.next())
-        create_product(code='a'*30, name=name.next())
-        assert_raises(db.ValidationError,
-                create_product, code='a'*31, name=name.next())
+        assert_invalid(create_product, code=None,  name=names.next())
+        assert_invalid(create_product, code='',    name=names.next())
+        assert_invalid(create_product, code='aa',  name=names.next())
+        create_product(code='aaa',  name=names.next())
+        create_product(code='a'*30, name=names.next())
+        assert_invalid(create_product, code='a'*31, name=names.next())
         
 
     def it_must_have_a_name_between_3_and_255_characters_long(self):
-        assert_equal(self.p.name, "Shoplifter T-Shirt")
+        assert_equal(self.p.name, self.name)
 
-        assert_raises(db.ValidationError, self._save_with, name=None)
-        assert_raises(db.ValidationError, self._save_with, name='')
-        assert_raises(db.ValidationError, self._save_with, name='aa')
-        assert_raises(db.ValidationError, self._save_with, name='a'*256)
+        assert_invalid(self.resave, name=None)
+        assert_invalid(self.resave, name='')
+        assert_invalid(self.resave, name='aa')
+        assert_invalid(self.resave, name='a'*256)
 
     def it_must_have_a_slug_between_3_and_30_characters_long(self):
-        assert_equal(self.p.slug, "shoplifter-t-shirt")
-        assert_raises(db.ValidationError, self._save_with, name=None)
-        assert_raises(db.ValidationError, self._save_with, name='')
-        assert_raises(db.ValidationError, self._save_with, slug='aa')
-        assert_raises(db.ValidationError, self._save_with, slug='a'*31)
+        assert_equal(self.p.slug, self.slug)
+        assert_invalid(self.resave, name=None)
+        assert_invalid(self.resave, name='')
+        assert_invalid(self.resave, slug='aa')
+        assert_invalid(self.resave, slug='a'*31)
 
     def it_should_auto_generate_slug_from_name_when_slug_is_blank(self):
-        self._save_with(slug=None)
-        assert_equal(self.p.slug, "shoplifter-t-shirt")
+        self.resave(slug=None)
+        assert_equal(self.p.slug, self.slug)
 
-        self._save_with(slug='')
-        assert_equal(self.p.slug, "shoplifter-t-shirt")
+        self.resave(slug='')
+        assert_equal(self.p.slug, self.slug)
 
     def it_can_have_a_description(self):
         assert_equal(self.p.description, None)
 
-        self._save_with(description="some description")
+        self.resave(description="some description")
         assert_equal(self.p.description, "some description")
 
     def it_can_have_a_meta_description(self):
         assert_equal(self.p.meta_description, None)
 
-        self._save_with(meta_description="some meta description")
+        self.resave(meta_description="some meta description")
         assert_equal(self.p.meta_description, "some meta description")
 
     def it_can_have_meta_keywords(self):
         assert_equal(self.p.meta_keywords, None)
 
-        self._save_with(meta_keywords="meta, keywords")
+        self.resave(meta_keywords="meta, keywords")
         assert_equal(self.p.meta_keywords, "meta, keywords")
 
     def it_has_a_static_slugify_method(self):
